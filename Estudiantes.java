57e54ea6eec85013fb6ed7acdb1f8a67ed15971e
Andrés Tinoco <andrestinoco06@outlook.com>
2018-02-19 14:06:31 -0500

    Estudiantes.java
    Calcula el porcentaje de estudiantes menores de edad

---
+++ Estudiantes.java
@@ -0,0 +1,9 @@
+public class Estudiantes {
+    public static void main(String[] args) {      
+      int estudiantes = 240;
+      int menores = 35;
+      int porcentaje;     
+      porcentaje = (35*100) /240;
+      System.out.println ("El porcentaje de estudiantes menores de edad es el: "+porcentaje+ "%");
+    }
+}
